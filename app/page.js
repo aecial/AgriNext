f475c1889ccc060e1923aea68e2df3cfbebbac48
Teddy Pascual <teddylayson2000@gmail.com>
2023-11-06 10:41:52 +0800

    added user schema and login page

--- app/page.js
+++ app/page.js
@@ -2,10 +2,12 @@ import Image from "next/image";
 import Button from "@mui/joy/Button";
 import Add from "@mui/icons-material/Add";
 import Table from "@mui/joy/Table";
+import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h1>Home PAGE</h1>
+      <h1>Landing Page</h1>
+      <Link href="/login">Login</Link>
     </main>
   );
 }
