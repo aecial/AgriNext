129c8d7ba19010ae50fd45dc06790b5497998cc3
Teddy Pascual <teddylayson2000@gmail.com>
2023-11-17 19:03:25 +0800

    added bcrypt

--- prisma/migrations/20231102093107_crop_type/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "cropTypes" (
-    "id" SERIAL NOT NULL,
-    "crop_type" TEXT NOT NULL,
-    "image" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "cropTypes_pkey" PRIMARY KEY ("id")
-);
